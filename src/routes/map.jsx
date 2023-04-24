49e298c6f8a0982bc5f3f2d5c33fa580265be27f
embersee <philipp@shiyanov.com>
2023-04-24 20:58:31 +0300

    fix react router dom & modal crashes

--- src/routes/map.jsx
+++ src/routes/map.jsx
@@ -5,67 +5,68 @@ import Modal from "../components/modal/Modal";
 import MapForm from "../components/MapForForm";
 
 export default function Map() {
-
-  const [modalActive, setModalActive] = useState(false)
+  const [modalActive, setModalActive] = useState(false);
 
   const points = [
     {
-      id: '1',
+      id: "1",
       coordinates: [59.836172, 30.164347],
-      title: 'Скейт-парк у речки',
-      description: 'Скейтпарк от компании FK-Ramps. Год постройки: 2023',
-      photo: 'https://cdnstatic.rg.ru/uploads/images/2022/08/29/1_dji_0143_402.jpg'
+      title: "Скейт-парк у речки",
+      description: "Скейтпарк от компании FK-Ramps. Год постройки: 2023",
+      photo:
+        "https://cdnstatic.rg.ru/uploads/images/2022/08/29/1_dji_0143_402.jpg",
     },
     {
       id: "2",
       coordinates: [55.831903, 37.411961],
-      title: "Пристань Метеоров"
+      title: "Пристань Метеоров",
     },
     {
       id: "3",
       coordinates: [55.763338, 37.565466],
-      title: "Парк Ваккасалми"
+      title: "Парк Ваккасалми",
     },
-
   ];
 
-    return (
-      <>
-        <div className="flex flex-grow">
-          <Link className="m-auto mt-[2%] text-5xl" to = {`/`}>Skate Map</Link>
-          <div className='absolute top-[2%] right-[2%] border-2 h-12 w-12 border-gray-800 rounded-full m-auto'></div>
-        </div>
-        <nav className="flex items-center m-[2%]">
-            <Link className="a" to = {`/map`}>Карта спотов</Link>
-            <Link className="a" to = {`/school`}>Школа трюков</Link>
-            <a className="a">Блог</a>
-            <a className="a">Правила скейтпарков</a>
-        </nav>
-        <div className="w-[96%] m-auto">
-          <button className='addSpot' onClick={() => setModalActive(true)}>Добавить спот</button>
-          <YaMap points={points}/>
-        </div>
-        <Modal id="modal" active={modalActive} setActive={setModalActive}>
-          <div className="flex flex-start items-center justify-center">
-            <div className="w-1/2 m-[5%]">
-              <MapForm/>
-            </div>
-            <div className="flex flex-col items-center w-1/2">
-              <h1 className="text-3xl">Заполните форму</h1>
-              <h2 className="text-xl mt-3">Введите вашу почту:</h2>
-              <input className="border-2 border-gray-800 rounded-md p-1 mt-3 w-2/3"></input>
-              <h2 className="text-xl mt-3">Введите название спота:</h2>
-              <input className="border-2 border-gray-800 rounded-md p-1 mt-3 w-2/3"></input>
-              <h2 className="text-xl mt-3">Введите описание спота:</h2>
-              <textarea className="border-2 border-gray-800 rounded-md p-1 mt-3 w-2/3 h-52 resize-none"></textarea>
-              <h2 className="text-xl mt-3">Добавьте фото:</h2>
-              <input type='file' className="p-1 mt-[3%] w-2/3"></input>
-              <button 
-                className="border-2 border-gray-800 flex items-center justify-center rounded-md p-1 mt-[5%] w-1/3"
-                onClick={() =>{setModalActive(false); const element = document.getElementById('modal'); element.remove()}}>Отправить</button>
-            </div>
+  return (
+    <>
+      <div className="w-[96%] m-auto">
+        <button className="addSpot" onClick={() => setModalActive(true)}>
+          Добавить спот
+        </button>
+        <YaMap points={points} />
+      </div>
+      {/* 
+        Где-то здесь есть ошибка с библиотеки, что возвращяет что-то из useEffect и из-за этого крашиться
+      
+      <Modal id="modal" active={modalActive} setActive={setModalActive}>
+        <div className="flex flex-start items-center justify-center">
+          <div className="w-1/2 m-[5%]">
+            <MapForm />
           </div>
-        </Modal>
-      </>
-    );
-  }
+          <div className="flex flex-col items-center w-1/2">
+            <h1 className="text-3xl">Заполните форму</h1>
+            <h2 className="text-xl mt-3">Введите вашу почту:</h2>
+            <input className="border-2 border-gray-800 rounded-md p-1 mt-3 w-2/3"></input>
+            <h2 className="text-xl mt-3">Введите название спота:</h2>
+            <input className="border-2 border-gray-800 rounded-md p-1 mt-3 w-2/3"></input>
+            <h2 className="text-xl mt-3">Введите описание спота:</h2>
+            <textarea className="border-2 border-gray-800 rounded-md p-1 mt-3 w-2/3 h-52 resize-none"></textarea>
+            <h2 className="text-xl mt-3">Добавьте фото:</h2>
+            <input type="file" className="p-1 mt-[3%] w-2/3"></input>
+            <button
+              className="border-2 border-gray-800 flex items-center justify-center rounded-md p-1 mt-[5%] w-1/3"
+              onClick={() => {
+                setModalActive(false);
+                const element = document.getElementById("modal");
+                element.remove();
+              }}
+            >
+              Отправить
+            </button>
+          </div>
+        </div>
+      </Modal> */}
+    </>
+  );
+}
--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,18 +1,28 @@
 import { Link } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 
 export default function Root() {
-    return (
+  return (
+    <>
+      <div className="flex flex-grow">
+        <Link className="m-auto mt-[2%] text-5xl" to={`/`}>
+          Skate Map
+        </Link>
+        <div className="absolute top-[2%] right-[2%] border-2 h-12 w-12 border-gray-800 rounded-full m-auto"></div>
+      </div>
+      <nav className="flex items-center m-[2%]">
+        <Link className="a" to="/map">
+          Карта спотов
+        </Link>
+        <Link className="a" to="/school">
+          Школа трюков
+        </Link>
+        <a className="a">Блог</a>
+        <a className="a">Правила скейтпарков</a>
+      </nav>
       <>
-        <div className="flex flex-grow">
-          <Link className="m-auto mt-[2%] text-5xl" to = {`/`}>Skate Map</Link>
-          <div className='absolute top-[2%] right-[2%] border-2 h-12 w-12 border-gray-800 rounded-full m-auto'></div>
-        </div>
-        <nav className="flex items-center m-[2%]">
-            <Link className="a" to = {`/map`}>Карта спотов</Link>
-            <Link className="a" to = {`/school`}>Школа трюков</Link>
-            <a className="a">Блог</a>
-            <a className="a">Правила скейтпарков</a>
-        </nav>
+        <Outlet />
       </>
-    );
-  }
+    </>
+  );
+}
